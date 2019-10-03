de22915080689bdca1c9b6760a40af10d07f24c0
amrik13 <amrik5188@gmail.com>
2019-10-03 17:50:20 +0530

    Initail Commit

---
+++ app/src/main/java/com/amriksinghpadam/pageviewerdemo2/MainActivity.java
@@ -0,0 +1,110 @@
+package com.amriksinghpadam.pageviewerdemo2;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+import android.text.Html;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private ViewPager viewPager;
+    Button nextBtn,previousBtn;
+    private TextView[] dot;
+    MyAdapter adapter;
+    LinearLayout dotLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        viewPager = findViewById(R.id.viewPagerId);
+        nextBtn = findViewById(R.id.nextBtnId);
+        previousBtn = findViewById(R.id.previousBtnId);
+        dotLayout = findViewById(R.id.dotLayout);
+
+        adapter = new MyAdapter(this,nextBtn,previousBtn);
+        viewPager.setAdapter(adapter);
+
+        nextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                viewPager.setCurrentItem(viewPager.getCurrentItem()+1);
+            }
+        });
+
+        previousBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                viewPager.setCurrentItem(viewPager.getCurrentItem()-1);
+            }
+        });
+
+        addDot();
+
+        viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+
+                // call dot method
+                if (position != 0 && position != (dot.length - 1)) {
+                    previousBtn.setVisibility(View.VISIBLE);
+                    nextBtn.setText("Next");
+                    dot[position + 1].setTextColor(getResources().getColor(R.color.inactiveDotColor));
+                    dot[position - 1].setTextColor(getResources().getColor(R.color.inactiveDotColor));
+                }
+                if (position == dot.length - 1) {
+                    dot[position - 1].setTextColor(getResources().getColor(R.color.inactiveDotColor));
+                    nextBtn.setText("Finish");
+                }
+                if (position == 0) {
+                    dot[position + 1].setTextColor(getResources().getColor(R.color.inactiveDotColor));
+                    previousBtn.setVisibility(View.INVISIBLE);
+                }
+//                for(int i=0;i<dot.length;i++){
+//                    dot[i].setTextColor(getResources().getColor(R.color.inactiveDotColor));
+//                }
+                dot[position].setTextColor(getResources().getColor(R.color.activeDotColor));
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+
+        });
+    }
+
+    public void addDot(){
+        dot = new TextView[adapter.getCount()];
+        for(int i = 0; i<adapter.getCount();i++){
+            dot[i] = new TextView(this);
+            dot[i].setText(Html.fromHtml("&#8226"));
+            dot[i].setTextSize(30);
+            dotLayout.addView(dot[i]);
+            dot[i].setTextColor(getResources().getColor(R.color.inactiveDotColor));
+        }
+        dot[0].setTextColor(getResources().getColor(R.color.activeDotColor));
+
+        if(viewPager.getCurrentItem()==0){
+            previousBtn.setVisibility(View.INVISIBLE);
+        }
+        if(viewPager.getCurrentItem()== dot.length - 1){
+            nextBtn.setVisibility(View.INVISIBLE);
+        }
+
+    }
+
+
+
+
+
+}
---
+++ app/src/main/java/com/amriksinghpadam/pageviewerdemo2/MyAdapter.java
@@ -0,0 +1,59 @@
+package com.amriksinghpadam.pageviewerdemo2;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+
+public class MyAdapter extends PagerAdapter {
+
+    private String[] str = {"1","2","3","4","5"};
+    private Button nextBtn;
+    private Button previousBtn;
+    private Context context;
+
+
+    MyAdapter(Context context,Button nextBtn, Button previousBtn){
+        this.nextBtn = nextBtn;
+        this.previousBtn = previousBtn;
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() { return str.length; }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view == object;
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.view_pager_item,container,false);
+        bindView(view,position,container);
+        return view;
+    }
+
+    public void bindView(View view, int position, final ViewGroup container){
+        TextView textView = view.findViewById(R.id.itemTextId);
+        textView.setText(str[position]);
+        container.addView(view);
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((View) object);
+    }
+
+
+
+
+
+}
